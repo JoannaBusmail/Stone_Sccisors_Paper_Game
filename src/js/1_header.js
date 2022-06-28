a11c3665db7ca2b924a954bff17e5885d2dc5984
JoannaBusmail <joanna.busmail@gmail.com>
2022-06-28 12:38:33 +0200

    adding animation and styles

---
+++ src/js/1_header.js
@@ -0,0 +1,7 @@
+'use strict';
+const headerRock = (document.querySelector('.js-imgHeaderStone').src =
+  '../assets/images/rock.png');
+const headerScissors = (document.querySelector('.js-imgHeaderScissors').src =
+  '../assets/images/tijeras.png');
+const headerPaper = (document.querySelector('.js-imgHeaderPaper').src =
+  'assets/images/paper.png');

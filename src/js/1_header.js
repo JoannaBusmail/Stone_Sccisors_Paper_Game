4bf2d4bf5bed8e45e11b8f6b20aca970f894a386
JoannaBusmail <joanna.busmail@gmail.com>
2022-06-30 13:04:19 +0200

    fixing image routes

--- src/js/1_header.js
+++ src/js/1_header.js
@@ -1,7 +1,7 @@
 'use strict';
 const headerRock = (document.querySelector('.js-imgHeaderStone').src =
-  '../assets/images/rockvertical.png');
+  'assets/images/rockvertical.png');
 const headerScissors = (document.querySelector('.js-imgHeaderScissors').src =
-  '../assets/images/scissorvertical.png');
+  'assets/images/scissorvertical.png');
 const headerPaper = (document.querySelector('.js-imgHeaderPaper').src =
   'assets/images/papervertical.png');
